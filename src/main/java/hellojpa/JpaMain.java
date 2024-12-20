0ca63c6106f85f2c8fa6384330caddf7f5236115
SeongHwan <aiya0422@gmail.com>
2024-12-20 15:54:33 +0900

    Hello JPA - 애플리케이션 개발

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -9,8 +11,46 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
-        em.close();
+        try {
+            //생성
+            //Member member = new Member();
+            //member.setId(1L);
+            //member.setName("HelloA");
+            //em.persist(member);
+
+            //조회
+            //Member findMember = em.find(Member.class, 1L);
+            //System.out.println("findMember.Id = " + findMember.getId());
+            //System.out.println("findMember.Name = " + findMember.getName());
+
+            //삭제
+            //Member findMember = em.find(Member.class, 1L);
+            //em.remove(findMember);
+
+            //수정
+            //Member findMember = em.find(Member.class, 1L);
+            //findMember.setName("HelloJPA");
+
+            //JPQL
+            //List<Member> result = em.createQuery("select m from Member as m", Member.class)
+            //        .setFirstResult(1)
+            //        .setMaxResults(8)
+            //        .getResultList();
+            //
+            //for (Member member : result) {
+            //    System.out.println("member = " + member);
+            //}
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
